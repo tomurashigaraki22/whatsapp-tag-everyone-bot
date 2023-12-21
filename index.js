ba2be8313bb97e2945fb8a22ca0be5e4bdbe8ff4
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2023-12-21 10:40:53 +0000

    awd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js")
 const fs = require('fs');
 const Anime_Images = require('anime-images-api')
+const axios = require('axios')
 const API = new Anime_Images()
 const https = require('https');
 const qrcode = require("qrcode-terminal")
@@ -70,11 +71,10 @@ client.on("message_create", async (message) => {
 }
 
 	if (message.body === '/quote') {
-		fetch('https://api.quotable.io/quotes/random')
-		.then((response) => response.json())
+		axios.get('https://api.quotable.io/quotes/random')
 		.then((quotes) => {
-			console.log(quotes)
-			const quote = quotes[0]
+			console.log(quotes.data)
+			const quote = quotes.data[0]
 			const author = quote.author
 			const content = quote.content
 			chat.sendMessage(`Quote: ${content} -${author}`)
@@ -85,13 +85,16 @@ client.on("message_create", async (message) => {
 		})
 	}
 
-	if (message.body === '/anime-quote'){
-		fetch("https://animechan.xyz/api/random")
-          .then((response) => response.json())
+	if (message.body === '/leader-quote'){
+		axios.get("https://api.api-ninjas.com/v1/quotes?category=leadership", {
+			headers: {
+				'X-Api-Key': 'njDYG76guOh1EZec+GnTvg==sVv5HD0IxXCdI2gl'
+			  },
+		})
           .then((quote) => {
 			console.log(quote)
-			const quotec = quote.character
-			const quotecont = quote.quote
+			const quotec = quote.data[0].author
+			const quotecont = quote.data[0].quote
 			chat.sendMessage(`Anime Quote:
 ${quotecont}	
 -${quotec}`)
@@ -99,10 +102,10 @@ ${quotecont}
 	}
 
 	if (message.body === '/waifu') {
-		fetch('https://api.waifu.pics/sfw/waifu')
-			.then((response) => response.json())
+		axios.get('https://api.waifu.pics/sfw/waifu')
 			.then(async (pic) => {
-				const url = await MessageMedia.fromUrl(pic.url);
+				console.log(pic)
+				const url = await MessageMedia.fromUrl(pic.data.url);
 
 				chat.sendMessage(url, {
 					caption: 'meme',
@@ -110,17 +113,185 @@ ${quotecont}
 			});
 	}
 
-	if (message.body === '/hug'){
-		fetch('https://api.otakugifs.xyz/gif?reaction=hug&format=gif')
-		.then((response) => response.json())
-		.then(async (pic) => {
-			const url = await MessageMedia.fromUrl(pic.url)
+	if (message.body === '/hug') {
+		axios.get('https://api.otakugifs.xyz/gif?reaction=hug&format=gif')
+			.then(async (pic) => {
+				console.log(pic);
+				const url = await MessageMedia.fromUrl(pic.data.url);
+	
+				// Check if there is a quoted message
+				if (message.hasQuotedMsg) {
+					const quotedMsg = await message.getQuotedMessage();
+					const huggedPerson = quotedMsg.author || quotedMsg.from;
+					const mauthor = message.author.split('@')
+					const mauthor2 = `@+${mauthor[0]}`
+					const mfrom = message.to.split('@')
+					const mfrom2 = `@+${mfrom}`
+	
+					// Reply with the hug GIF and mention the person who sent the quoted message
+					chat.sendMessage(url, {
+						caption: `${mauthor2} hugged ${mfrom2}`
+					});
+				} else {
+					// If no quoted message, send the hug response to the current chat
+					chat.sendMessage(url, {
+						caption: 'Hug time!'
+					});
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				chat.sendMessage('Error fetching hug GIF.');
+			});
+	}
+	
+	
 
-			chat.sendMessage(url)
+	if (message.body === '/joke'){
+		axios.get('https://api.api-ninjas.com/v1/jokes?limit=1', {
+			headers: {
+				'X-Api-Key': 'njDYG76guOh1EZec+GnTvg==sVv5HD0IxXCdI2gl'
+			  },
 		})
+		.then(async (jok) => {
+			console.log(jok)
+			const joke = jok.data[0].joke
+			chat.sendMessage(`Joke: ${joke}`)
+		})
+	}
+
+	if (message.body === '/cuddle') {
+		axios.get('https://api.waifu.pics/sfw/cuddle')
+			.then(async (pic) => {
+				console.log(pic);
+	
+				// Check if the received media is a GIF
+				if (pic.data.url.endsWith('.gif')) {
+					const gifBuffer = await axios.get(pic.data.url, { responseType: 'arraybuffer' });
+					const url = new MessageMedia('image/gif', gifBuffer.data.toString('base64'));
+					console.log(gifBuffer)
+	
+					chat.sendMessage(url, {
+						caption: 'meme',
+						sendVideoAsGif: true
+					});
+				} else {
+					// If it's not a GIF, handle it as an image
+					const url = await MessageMedia.fromUrl(pic.data.url);
+					console.log('Not a gif')
+					chat.sendMessage(url, {
+						caption: 'meme',
+					});
+				}
+			});
+	}
+	if (message.body === '/waifu-n'){
+		fetch('https://api.waifu.pics/nsfw/neko')
+		.then((response) => {
+			if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then(async (data) => {
+			const url = await MessageMedia.fromUrl(data.url);
+			chat.sendMessage(url, {
+			caption: 'NSFW'
+			});
+		})
+		.catch((error) => {
+			console.error('Error fetching NSFW neko:', error);
+		});
 
 	}
+	if (message.body === '/husbando'){
+		fetch(`https://nekos.best/api/v2/husbando`)
+			.then((response) => response.json())
+			.then(async (pic) => {
+				console.log(pic.results[0].url)
+				const url = await MessageMedia.fromUrl(pic.results[0].url)
+				chat.sendMessage(url, {
+					caption: '.'
+				})
+			})
+	}
+
+	if (message.body === '/anime-rec'){
+		const animes = []
+		fetch('https://api.animethemes.moe/anime')
+		.then((response) => response.json())
+		.then(async (anime) => {
+			for (let i = 0; i < 5 && i < anime.length; i++) {
+				const animea = anime[i]
+				// Log or process each anime recommendation
+				chat.sendMessage(`Recommendation ${i + 1}: ${animea.name}
+				Description: ${animea.synopsis}
+				Year: ${animea.year}`)
+				console.log(anime[i])
+				animes.push()
+	  
+				// You can send the anime recommendation as a message or perform other actions
+				// Example: chat.sendMessage(`Recommendation ${i + 1}: ${anime.name}`);
+			  }
+
+		})
+	}
+
+	if (message.body === '/anime-img'){
+		axios.get(`https://api.nekosapi.com/v3/images`)
+		.then(async (data) => {
+			const randomnum = getRandomNumber(1, 100)
+			console.log(data)
+			console.log(data.items[randomnum].image_url)
+			const url = await MessageMedia.fromUrl(data.items[randomnum].image_url)
+			chat.sendMessage(url, {
+				caption: '.',
+			})
+		})
+	}
+
+	if (message.body === '/contact-name') {
+		try {
+			const contact = await message.to
+			console.log(contact);
+			
+			const contactName = contact.pushname; // Get the name from the contact object
+			
+			chat.sendMessage(`Contact name: ${contactName}`);
+		} catch (error) {
+			console.error("Error fetching contact:", error);
+			chat.sendMessage("Error fetching contact.");
+		}
+	}
+
+	if (message.body === '!quoteinfo' && message.hasQuotedMsg) {
+        const quotedMsg = await message.getQuotedMessage();
+
+        quotedMsg.reply(`
+            ID: ${quotedMsg.id._serialized}
+            Type: ${quotedMsg.type}
+            Author: ${quotedMsg.author || quotedMsg.from}
+            Timestamp: ${quotedMsg.timestamp}
+            Has Media? ${quotedMsg.hasMedia}
+        `);
+    }
 })
+
+client.on('group_join', (notification) => {
+	// User has joined or been added to the group.
+	const participant = notification.id.participant;
+	const participantWithoutSuffix = participant.split('@')[0];
+	const welcomeMessage = '\n' + `Please introduce yourself and 3 of your favourite animes`;
+
+	const chat = notification.getChat()
+	console.log('join', participantWithoutSuffix);
+	console.log('Joined', chat.name)
+	notification.reply(`Welcome @+${participantWithoutSuffix}${welcomeMessage}`);
+  });
 	
+
+  function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 client.initialize()
